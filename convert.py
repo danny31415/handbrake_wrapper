1016ede8141a21bf250c9761b99f67915d7efb8e
Danny Lynch <danny31415@gmail.com>
2010-12-12 23:13:17 -0600

    Added command line argument for path to HandBrakeCLI with a reasonable default

--- convert.py
+++ convert.py
@@ -12,12 +12,13 @@ from optparse import OptionParser
 #  It is on the todo list to fix later
 
 #example
-#z:\Apps\Python31\python.exe z:\Apps\scripts\convert2.py -i "Z:\Videos\TV\SomeShow" ExtraTextForSomeShow -o "z:\Videos\ConvertedOutput"
+#z:\Apps\Python31\python.exe z:\Apps\scripts\convert2.py -i 'Z:\Videos\TV\SomeShow' ExtraTextForSomeShow -o 'z:\Videos\ConvertedOutput'
 
 
 parser = OptionParser()
-parser.add_option("-i", "--input-directory",  dest="in_dirs", action="append", help="directory that has the files to be converted", metavar="IN_DIR", nargs=2, type="string")
-parser.add_option("-o", "--output-directory", dest="out_dir", action="store",  help="output directory for the converted file",      metavar="OUT_DIR")
+parser.add_option('-i', '--input-directory',  dest='in_dirs', action='append', help='directory that has the files to be converted', metavar='IN_DIR', nargs=2, type='string')
+parser.add_option('-o', '--output-directory', dest='out_dir', action='store',  help='output directory for the converted file',      metavar='OUT_DIR')
+parser.add_option('-e', '--executable-path', dest='exe_path', action='store',  help='The path to the HandBrakeCLI executable',      metavar='EXE_PATH')
 (options, args) = parser.parse_args()
 
 if options.out_dir == None:
@@ -28,19 +29,25 @@ for (in_dir,extra_tag) in options.in_dirs:
     for in_file in dir_listing:
         (out_file, extension) = os.path.splitext(in_file)
         out_file = extra_tag + out_file
-        if extension == ".avi":
-            out_file = out_file + ".mp4"
-            cmd  = os.path.join("z:","Apps","Handbrake","HandBrakeCLI.exe")
+        if extension == '.avi':
+            out_file = out_file + '.mp4'
+            #cmd  = os.path.join('z:','Apps','Handbrake','HandBrakeCLI.exe')
+            if options.exe_path:
+                cmd = options.exe_path
+            else:
+                cmd  = 'HandBrakeCLI'
             os.path.normpath(cmd)
         
             
             in_total_path  = os.path.join(in_dir, in_file)
             out_total_path = os.path.join(options.out_dir,out_file)
-            cmd += " -i \"%s\"" % in_total_path
-            cmd += " -o \"%s\"" % out_total_path
-            cmd += " --preset=\"iPhone & iPod Touch\"" 
-            print ("cmd is %s\n\n" % cmd)
-            print(subprocess.call(cmd))
+            cmd += ' -i \"%s\"' % in_total_path
+            cmd += ' -o \"%s\"' % out_total_path
+            cmd += ' --preset=\"iPhone & iPod Touch\"'
+            print ('cmd is %s\n\n' % cmd)
+            #for some reason subprocess.call doesnt work here when I have the command line arguments
+            #added onto the end. Weird. I changed it to an os.system call for now instead
+            print(os.system(cmd))
     
 
 
@@ -51,6 +58,6 @@ for (in_dir,extra_tag) in options.in_dirs:
 #Convert the movie first to a temporary file, then copy if it finished successfully
 
 #example
-#z:\Apps\Python31\python.exe z:\Apps\scripts\convert.py -i "Z:\Videos\TV\SomeShow" ExtraTextForSomeShow -o "z:\Videos\ConvertedOutput"
+#z:\Apps\Python31\python.exe z:\Apps\scripts\convert.py -i 'Z:\Videos\TV\SomeShow' ExtraTextForSomeShow -o 'z:\Videos\ConvertedOutput'
 #
-#-i "Z:\Videos\The Teaching Company\TTC VIDEO - Games People Play Game Theory in Life, Business and Beyond (compressed)" GamesPeoplePlay -i "Z:\Videos\The Teaching Company\TTC Video - Building Great Sentences - Exploring the Writer's Craft" BuildingGreatSentences -i "Z:\Videos\The Teaching Company\FCL Series - TTC - Contemporary Economic Issues" ContemoraryEconomicIssues -o "Z:\Videos\DannyConverted"
+#-i 'Z:\Videos\The Teaching Company\TTC VIDEO - Games People Play Game Theory in Life, Business and Beyond (compressed)' GamesPeoplePlay -i 'Z:\Videos\The Teaching Company\TTC Video - Building Great Sentences - Exploring the Writer's Craft' BuildingGreatSentences -i 'Z:\Videos\The Teaching Company\FCL Series - TTC - Contemporary Economic Issues' ContemoraryEconomicIssues -o 'Z:\Videos\DannyConverted'
